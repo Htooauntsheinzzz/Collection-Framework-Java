7883b344256ad1540c37f96dfa93dc442ad014a6
Htoo Aunt Shein <htooauntshein@Htoos-MacBook-Pro.local>
2026-08-12 19:56:36 +0700

    Enum Set&Map, hashtable, treemap Lesson

---
+++ src/enumset/Days.java
@@ -0,0 +1,26 @@
+package enumset;
+
+
+/*
+ * 
+ * Enum Set -> Special for Enum Types
+ * 			-> Unique 
+ * 			-> Dont allow null key or value
+ * 			-> Treemap -> Maintains Ascending Order
+ * 
+ */
+
+
+
+
+public enum Days {
+	
+	SUNDAYS,
+	MONDAYS,
+	TUESDAYS,
+	WEDNESDAYS,
+	THURSDAYS,
+	FIRDAYS,
+	SATURDAYS
+
+}
